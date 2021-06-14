952a24f013881dbb7716e8d71f7c2b0e2286cd72
Daniel Vogel <danielcvt@gmail.com>
2021-06-14 02:26:56 -0300

    hero-py

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,11 +15,11 @@ return(
     alignItems="center"
     bgGradient="linear(to-l, #7928CA, #FF0080)"
     bgClip="text"
-    py="8"
+    py={{md: "1", lg: "8"}}
   >
         {/* <Image width="150px" height="150px" borderRadius="full" src="https://images.prismic.io/carolinaprocaci-levecriar/e3fedded-bd6c-4af9-9aa3-9625bcc57968_187812270_371317947654797_506182409226668856_n.jpg?auto=compress,format" alt="CarolinaProcaci"/> */}
 <Img borderRadius="full" src="https://images.prismic.io/carolinaprocaci-levecriar/e3fedded-bd6c-4af9-9aa3-9625bcc57968_187812270_371317947654797_506182409226668856_n.jpg?auto=compress,format" boxSize="150px"/>
-    <Heading fontSize={"7vw"}>{title}</Heading>
+    <Heading fontSize={"6vw"}>{title}</Heading>
     <Text       color={color[colorMode]}
 >
 Consultora em Amamentação
