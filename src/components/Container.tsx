f3a8814743ccda4db20fc3be3a8649c5f8cc689a
Daniel Vogel <danielcvt@gmail.com>
2021-06-14 02:41:05 -0300

    container-flexStart

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,7 +10,7 @@ export const Container = (props: FlexProps) => {
     <Flex
       direction="column"
       alignItems="center"
-      justifyContent="space-between"
+      justifyContent="flex-start"
       bg={bgColor[colorMode]}
       color={color[colorMode]}
       {...props}
