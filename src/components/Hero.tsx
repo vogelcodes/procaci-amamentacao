fef4df5f6ee805bf54be54585b079c53a9c0e954
Daniel Vogel <danielcvt@gmail.com>
2021-06-14 01:43:54 -0300

    image-size-fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ export const Hero = ({ title }: { title: string }) => (
     pb="8"
   >
         {/* <Image width="150px" height="150px" borderRadius="full" src="https://images.prismic.io/carolinaprocaci-levecriar/e3fedded-bd6c-4af9-9aa3-9625bcc57968_187812270_371317947654797_506182409226668856_n.jpg?auto=compress,format" alt="CarolinaProcaci"/> */}
-<Img borderRadius="full" src="https://images.prismic.io/carolinaprocaci-levecriar/e3fedded-bd6c-4af9-9aa3-9625bcc57968_187812270_371317947654797_506182409226668856_n.jpg?auto=compress,format" height="150" width="150"/>
+<Img borderRadius="full" src="https://images.prismic.io/carolinaprocaci-levecriar/e3fedded-bd6c-4af9-9aa3-9625bcc57968_187812270_371317947654797_506182409226668856_n.jpg?auto=compress,format" boxSize="150px"/>
     <Heading fontSize="6vw">{title}</Heading>
   </Flex>
 )
